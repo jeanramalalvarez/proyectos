cf937b3c315a6227503f059ae4afa61b35386246
jean <jean@192.168.0.20>
2016-01-05 23:02:02 -0500

    borrando alter

--- veterinaria/GestionMatrizSeconReload/bd/alter_table_producto_20151208.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `bdmatriz_desa`.`tb_producto` 
-ADD COLUMN `str_tipoProducto` CHAR(1) NOT NULL DEFAULT 'P' COMMENT '' AFTER `dte_fechaModificacion`,
-ADD COLUMN `int_idTipoServicio` INT(11) NULL COMMENT '' AFTER `str_tipoProducto`;
